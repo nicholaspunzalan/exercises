3c60140ca66ea9b24bedac99f13962a23d031ebc
nicholas punzalan <dad@nicholass-mbp.lan>
2022-05-04 22:01:16 -0400

    added catch_the_number.rb

---
+++ code/loops_2/catch_the_number.rb
@@ -0,0 +1,11 @@
+counter = 0
+
+loop do
+  number = rand(100)
+  puts number
+  counter += 1
+  if number >= 0 && number <= 10
+    puts "Your winner is ##{number}, after #{counter} try/tries"
+    break
+  end
+end
